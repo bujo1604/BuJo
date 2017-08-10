7a3f4429c17b57205677bf715dff4e1e73e7bd3b
svetlanakaida <svetlanakaida@users.noreply.github.com>
2017-08-10 18:14:17 -0400

    routes for event, task, category added

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,13 @@
-const router = require('express').Router()
-module.exports = router
+const router = require('express').Router();
+module.exports = router;
 
-router.use('/users', require('./users'))
+router.use('/users', require('./users'));
+router.use('/categories', require('./category'));
+router.use('/tasks', require('./task'));
+router.use('/events', require('./event'));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
