a30513e912c351e320edd48f9c8bab3226ebda44
svetlanakaida <svetlanakaida@users.noreply.github.com>
2017-08-11 16:30:49 -0400

    got calendar

--- client/routes.js
+++ client/routes.js
@@ -4,7 +4,7 @@ import {Router} from 'react-router'
 import {Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
-import {Main, Login, Signup, UserHome, Insights} from './components'
+import {Main, Login, Signup, UserHome, Insights, MyCalendar} from './components';
 import {me} from './store'
 
 /**
@@ -31,6 +31,7 @@ class Routes extends Component {
                   {/* Routes placed here are only available after logging in */}
                   <Route path='/home' component={UserHome} />
                   <Route path='/insights' component={Insights} />
+                  <Route exact path='/calendar' component={MyCalendar} />
                 </Switch>
             }
             {/* Displays our Login component as a fallback */}
