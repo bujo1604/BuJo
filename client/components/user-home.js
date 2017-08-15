70c73efc5ea7627d89f9ef0716f3a77149c53d64
Emily McCarthy <emccarthy510@yahoo.com>
2017-08-15 10:52:34 -0400

    new svg character

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-
+import svgCharacter from './svgCharacter'
 /**
  * COMPONENT
  */
+
+ function colorPick(){
+  return "blue";
+}
+
 export const UserHome = (props) => {
   const {email} = props
 
@@ -12,7 +17,33 @@ export const UserHome = (props) => {
     <div>
       <h3>Welcome, {email}</h3>
       <img src="./bujoavatar.gif"></img>
-    </div>
+        <svg width="300" height="300">
+            
+            <rect x="0" y="0" width="300" height="300" fill="white" />
+            <ellipse cx="200" cy="145" rx="8" ry="100" fill="Gainsboro" transform = "rotate(45 200 145)" />
+            <ellipse cx="100" cy="145" rx="8" ry="100" fill="Gainsboro" transform = "rotate(-45 100 145)" />
+            <ellipse cx="175" cy="210" rx="10" ry="50" fill="Gainsboro" /> 
+            <ellipse cx="125" cy="210" rx="10" ry="50" fill="Gainsboro" />
+
+            <ellipse cx="150" cy="125" rx="50" ry="45" fill="Gainsboro" />
+            <rect x="100" y="125" width="100" height="100" fill={colorPick()} />
+            
+            <ellipse cx="150" cy="125" rx="50" ry="10" fill="Gainsboro" />
+            <ellipse cx="150" cy="225" rx="50" ry="10" fill={colorPick()} />
+            <ellipse cx="100" cy="175" rx="10" ry="50" fill={colorPick()} />
+            <ellipse cx="200" cy="175" rx="10" ry="50" fill={colorPick()} />
+            <circle cx="175" cy="120" r="3" fill="red"/>
+            <circle cx="125" cy="120" r="3" fill="red"/>
+            <line x1="147" y1="125" x2="153" y2="125" stroke="black"  />
+            <line x1="170" y1="105" x2="180" y2="105" stroke="black"  />
+            <line x1="120" y1="105" x2="130" y2="105" stroke="black"  />
+           <ellipse cx="200" cy="145" rx="10" ry="50" fill={colorPick()} transform = "rotate(45 200 145)" />
+             <ellipse cx="100" cy="145" rx="10" ry="50" fill={colorPick()} transform = "rotate(-45 100 145)" />
+            
+            
+        </svg>
+       
+      </div>
   )
 }
 
@@ -33,3 +64,4 @@ export default connect(mapState)(UserHome)
 UserHome.propTypes = {
   email: PropTypes.string
 }
+
