70c48172216d10a199972877fecb43b03e151b79
Lena Forti <lena@Lenas-MacBook-Pro.local>
2017-08-15 15:32:54 -0400

    functioning with glitches

--- client/components/scatter.js
+++ client/components/scatter.js
@@ -2,73 +2,10 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import * as d3 from 'd3';
 
-const myTasks = [
-  {
-    name: 'laundry',
-    categorory: 'home',
-    color: '#E91E63',
-    day: "20170801"
-  },
-  {
-    name: 'dishes',
-    categorory: 'home',
-    color: '#E91E63',
-    day: "20170801"
-  },
-  {
-    name: 'dishes',
-    categorory: 'home',
-    color: '#E91E63',
-    day: "20170802"
-  },
-  {
-    name: 'dishes',
-    categorory: 'home',
-    color: '#E91E63',
-    day: "20170802"
-  },
-  {
-    name: 'run',
-    categorory: 'exercise',
-    color: '#7C4DFF',
-    day: "20170802"
-  },
-  {
-    name: 'dinner',
-    categorory: 'social',
-    color: '#E57373',
-    day: "20170803"
-  },
-  {
-    name: 'redux',
-    categorory: 'learning',
-    color: '#2196F3',
-    day: "20170803"
-  },
-  {
-    name: 'redux',
-    categorory: 'learning',
-    color: '#2196F3',
-    day: "20170804"
-  },
-  {
-    name: 'redux',
-    categorory: 'learning',
-    color: '#2196F3',
-    day: "20170805"
-  },
-  {
-    name: 'redux',
-    categorory: 'learning',
-    color: '#2196F3',
-    day: "20170806"
-  }
-];
-
 function giveCountToTasks(tasks) {
   let count = 1
   tasks.forEach((task, i) => {
-    if (i > 0 && task.day === tasks[i - 1].day) {
+    if (i > 0 && task.date === tasks[i - 1].date) {
       count++
       task.count = count
     }
@@ -79,11 +16,7 @@ function giveCountToTasks(tasks) {
   })
 }
 
-giveCountToTasks(myTasks)
-
-
 //COMPONENT
-
 
 export class Scatter extends Component {
   constructor(props) {
@@ -92,13 +25,14 @@ export class Scatter extends Component {
     this.width = 300
     this.padding = 40
     this.parseTime = d3.timeParse('%Y%m%d')
+    this.countFunction = giveCountToTasks(this.props.tasks)
     //calculate min and max values of data for scale
-    this.xDomain = (tasks) => d3.extent(tasks, task => this.parseTime(task.day))
+    this.xDomain = (tasks) => d3.extent(tasks, task => this.parseTime(task.date))
     //for y axis, set the max to either be 10 or the max count
     this.yMax = (tasks) => Math.max(10, d3.max(tasks, task => task.count))
     //create scale with domain min and max and range of svg coordinates
-    this.xScale = d3.scaleTime().domain(this.xDomain(myTasks)).range([this.padding, this.width - this.padding])
-    this.yScale = d3.scaleLinear().domain([0, this.yMax(myTasks)]).range([this.height - this.padding, this.padding])
+    this.xScale = d3.scaleTime().domain(this.xDomain(this.props.tasks)).range([this.padding, this.width - this.padding])
+    this.yScale = d3.scaleLinear().domain([0, this.yMax(this.props.tasks)]).range([this.height - this.padding, this.padding])
     //create axis with scales
     this.xAxis = d3.axisBottom(this.xScale).ticks(7)
     this.yAxis = d3.axisLeft(this.yScale).ticks(3)
@@ -110,17 +44,17 @@ export class Scatter extends Component {
   }
 
   render() {
-
+    const {tasks} = this.props
     return (
       <div>
         <svg width={this.width} height={this.height}>
-          {myTasks.map((task, i) => (
+          {tasks.map((task, i) => (
             <g key={i}>
               <circle
                 r="10"
-                cx={this.xScale(this.parseTime(task.day))}
+                cx={this.xScale(this.parseTime(task.date))}
                 cy={this.yScale(task.count)}
-                fill={task.color}
+                fill={task.category.color.hex}
               />
             </g>)
           )}
--- client/store/tasks.js
+++ client/store/tasks.js
@@ -5,21 +5,18 @@ import axios from 'axios';
  * ACTION TYPES
  */
 const GET_TASK_LIST = 'GET_TASK_LIST';
-const GET_SINGLE_TASK = 'GET_SINGLE_TASK';
+// const GET_SINGLE_TASK = 'GET_SINGLE_TASK';
 
 /**
  * INITIAL STATE
  */
-const intialState = {
-    taskList: [],
-    singleTask: {}
-};
+const intialState = []
 
 /**
  * ACTION CREATORS
  */
 const getTaskList = (taskList) => ({type: GET_TASK_LIST, taskList});
-const getSingleTask = (singleTask) => ({type: GET_SINGLE_TASK, singleTask});
+// const getSingleTask = (singleTask) => ({type: GET_SINGLE_TASK, singleTask});
 
 /**
  * THUNK CREATORS
@@ -33,53 +30,50 @@ export function fetchTaskList (userId) {
     };
 }
 
-export function fetchSingleTask (taskId) {
-    return function thunk (dispatch){
-        return axios.get(`/api/tasks/${taskId}`)
-        .then(res => {
-          dispatch(getSingleTask(res.data));
-        })
-        .catch(error => { console.log(error) });
-    };
-}
+// export function fetchSingleTask (taskId) {
+//     return function thunk (dispatch){
+//         return axios.get(`/api/tasks/${taskId}`)
+//         .then(res => {
+//           dispatch(getSingleTask(res.data));
+//         })
+//         .catch(error => { console.log(error) });
+//     };
+// }
 
-export function createTask (task) {
-    return function thunk (dispatch){
-        return axios.post('/api/tasks', {task})
-        .then(res => dispatch(getSingleTask(res.data)))
-        .catch(error => { console.log(error) });
-    };
-}
+// export function createTask (task) {
+//     return function thunk (dispatch){
+//         return axios.post('/api/tasks', {task})
+//         .then(res => dispatch(getSingleTask(res.data)))
+//         .catch(error => { console.log(error) });
+//     };
+// }
 
-export function changeTask (taskId, task) {
-    return function thunk (dispatch){
-        return axios.put(`/api/tasks/${taskId}`, {task})
-        .then(res => dispatch(getSingleTask(res.data)))
-        .catch(error => { console.log(error) });
-    }
-}
+// export function changeTask (taskId, task) {
+//     return function thunk (dispatch){
+//         return axios.put(`/api/tasks/${taskId}`, {task})
+//         .then(res => dispatch(getSingleTask(res.data)))
+//         .catch(error => { console.log(error) });
+//     }
+// }
 
-export function deleteTask(taskId){
-    return function thunk(){
-        return axios.delete(`/api/tasks/${taskId}`)
-        .catch(error => { console.log( error) });
-    };
-}
+// export function deleteTask(taskId){
+//     return function thunk(){
+//         return axios.delete(`/api/tasks/${taskId}`)
+//         .catch(error => { console.log( error) });
+//     };
+// }
 
 /**
  * REDUCER
  */
 export default function (state = intialState, action) {
-  let newState = Object.assign({}, state);
   switch (action.type) {
     case GET_TASK_LIST:
-      newState.taskList = action.taskList;
-      break;
-    case GET_SINGLE_TASK:
-      newState.singleTask = action.singleTask;
-      break;
+      return action.taskList
+    // case GET_SINGLE_TASK:
+    //   newState.singleTask = action.singleTask;
+    //   break;
     default:
       return state;
   }
-  return newState;
 }
