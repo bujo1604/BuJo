c16fc0581fedf749cfab7002b72393760fb5391e
svetlanakaida <svetlanakaida@users.noreply.github.com>
2017-08-18 14:33:13 -0400

    fixed bug in update

--- client/components/tasks.js
+++ client/components/tasks.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { removeTask, fetchTasks, changeTask } from '../store';
 
-const Tasks = (props) => {
-    const { tasks, handleClick, user, changeStatus } = props;
+
+class Tasks extends Component {
+
+  constructor(props) {
+    super(props);
+     this.state = {
+           completed: false
+        }
+
+  }
+
+
+  render() {
+    const { tasks, handleClick, user, changeStatus } = this.props;
     return (
         <div>
             <h3 className="singleName-headings">Tasks</h3>
@@ -15,12 +27,13 @@ const Tasks = (props) => {
                             <span> {task.name} </span>
                         </div> :
                         <div >
-                            <span id={task.id} onClick={changeStatus(user) } style={{ color: `${task.category.color.hex}` }}> &#x25CF;</span>
+                            <span id={task.id} onClick={changeStatus(user)} style={{ color: `${task.category.color.hex}` }}> &#x25CF;</span>
                             <span> {task.name}
                             </span><button id={task.id} onClick={handleClick(user)} type='submit' >DELETE</button>
 
                         </div>
                     }
+
                 </div>
             ))}
         </div>
@@ -28,7 +41,7 @@ const Tasks = (props) => {
 
 }
 
-
+}
 
 
 const mapState = (state) => ({
@@ -38,7 +51,7 @@ const mapState = (state) => ({
 const mapDispatch = (dispatch) => {
     return {
         handleClick(user) {
-            
+
             return ((event) => {
 
                 event.preventDefault()
@@ -48,18 +61,19 @@ const mapDispatch = (dispatch) => {
             )
         },
         changeStatus(user) {
-            //add possiblity of changing from complete to complete 
+            //add possiblity of changing from complete to complete
+
             const updatedTask = {
                 status: 'complete',
             }
-            
+
             return (
                 (event) => {
                     // alert('are you sure?'); // if yes then change task
                     const taskId = event.target.id
                     event.preventDefault()
-                    dispatch(changeTask(taskId, updatedTask))
-                    dispatch(fetchTasks(user.id)) // needs to adjust the props recieve. componentWillRecieveProps 
+                    dispatch(changeTask(taskId, updatedTask, user.id))
+                    // dispatch(fetchTasks(user.id)) // needs to adjust the props recieve. componentWillRecieveProps
 
                 })
 
