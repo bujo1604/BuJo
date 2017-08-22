51db7ed37cabe1864748bdd62b0c47ced5955ec7
Emily McCarthy <emccarthy510@yahoo.com>
2017-08-22 15:29:39 -0400

    Able to add a future task

--- client/components/TaskWords.js
+++ client/components/TaskWords.js
@@ -1,5 +1,11 @@
-import React from 'react';
 
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchCategories } from '../store';
+import { Link } from 'react-router-dom';
+
+
+/*
 const TaskWords = (props) => {
     const { tasks } = props;
     
@@ -7,8 +13,8 @@ const TaskWords = (props) => {
         <div>
             {tasks.map((task, idx) => (
                     <div display="inline" key={idx}>
-                        
-                            <div key={idx} style={{ color: `${task.category.color.hex}`}} display="inline">{task.name}</div>
+                            <button>update</button>
+                            <div key={idx} style={{ color: `${task.category.color.hex}` }} display="inline">{task.name}</div>
                 </div>
             ))}
         </div>
@@ -16,3 +22,66 @@ const TaskWords = (props) => {
 }
 
 export default TaskWords;
+
+*/
+
+
+class TaskWords extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+componentDidMount(){
+    //this.props.loadCategories(this.props.user.id)
+}
+
+
+  render() {
+    const { tasks } = this.props;
+console.log(this.props, "this.props. in taskwords")
+    for(let i = 0; i < tasks.length; i++){
+        if(!!tasks.category){
+            console.log("has a category")
+        }
+        else{
+            console.log("doesnt have a category")
+        }
+    }
+    
+    return (
+        <div>
+            {tasks.map((task, idx) => (
+                    <div display="inline" key={idx}>
+                           
+                            <span key={idx} display="inline">{task.name}</span>
+                                <Link to={'/help'}>update</Link>
+                            </div>
+            ))}
+        </div>
+    )
+}
+
+    
+
+}
+
+const mapState = (state) => ({
+ //categories: state.categories,
+ user: state.user,
+});
+
+const mapDispatch = (dispatch) => {
+  return {
+      /*
+        loadCategories(userId) {
+            dispatch(fetchCategories(userId));
+
+
+        }, */
+  
+  };
+}
+
+export default connect(mapState, mapDispatch)(TaskWords);
+
